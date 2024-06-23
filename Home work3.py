5751e60f0bf80694f9ff4651d5a418caa8e7edf1
Egor <repingign@gmail.com>
2024-06-23 11:48:12 +0300

    Add files via upload

---
+++ Home work3.py
@@ -0,0 +1,8 @@
+name = input('Как Вас зовут? ')
+print('Приветствую тебя! ', name)
+age = input('Ваш возраст? ')
+print('Ваш возраст:', age, 'лет')
+age = int(age) + 1
+print('В следующем году вам будет:', age)
+is_student = True
+print('Поздравляю Вы студен!:', is_student)
